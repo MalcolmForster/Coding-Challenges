2af02039fdf1a1d3be8257aa32e9ea611040fce9
Malcolm <forster.malcolm@gmail.com>
2022-09-14 22:21:08 +1200

    Create #185 Easy.py
    Started 185

---
+++ Daily Coding Problems/#185 Easy.py
@@ -0,0 +1,16 @@
+#Given tw rectangles on 2D graph, return the area of their intersection
+#If no intersection, return 0
+
+# {
+#     "top_left": (1, 4),
+#     "dimensions": (3, 3) # width, height
+# }
+
+# {
+#     "top_left": (0, 5),
+#     "dimensions": (4, 3) # width, height
+# }
+
+#will return 6
+
+
